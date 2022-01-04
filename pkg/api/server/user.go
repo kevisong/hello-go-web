281217623b213edad2e262b455252243009ff402
KEVISONG <303043149@qq.com>
2022-01-04 22:40:48 +0800

    feat: add post user

--- pkg/api/server/user.go
+++ pkg/api/server/user.go
@@ -22,3 +22,14 @@ func userGet(c *gin.Context) {
 	}
 	c.JSON(http.StatusOK, models.Response{Code: 0, Message: "success", Data: user})
 }
+
+func userPost(c *gin.Context) {
+	user := models.User{}
+	err := c.BindJSON(&user)
+	if err != nil {
+		c.JSON(http.StatusOK, models.Response{Code: 1, Message: err.Error()})
+		return
+	}
+	// TODO: write to DB
+	c.JSON(http.StatusOK, models.Response{Code: 0, Message: "success", Data: user})
+}
