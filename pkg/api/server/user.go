abdb1e7c3a9f74ff815d8161ef5352ee8f202ec0
KEVISONG <303043149@qq.com>
2022-01-14 23:45:22 +0800

    feat: mod

--- pkg/api/server/user.go
+++ pkg/api/server/user.go
@@ -30,7 +30,6 @@ func userPost(c *gin.Context) {
 		c.JSON(http.StatusOK, models.Response{Code: 1, Message: err.Error()})
 		return
 	}
-	// TODO: write to DB
 	id, err := db.PostUser(user)
 	if err != nil {
 		c.JSON(http.StatusOK, models.Response{Code: 1, Message: err.Error()})
